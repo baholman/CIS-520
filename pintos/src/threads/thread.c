5a948a6dfe5a997c30d7787af95a4c3aa81c1d59
Blake Holman <baholman@ksu.edu>
2019-02-18 14:52:41 -0600

    works on bug

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -531,12 +531,15 @@ init_thread (struct thread *t, const char *name, int priority)
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
   t->magic = THREAD_MAGIC;
+  list_push_back(&all_list, &t->allelem);
+
+  t->init_priority = priority;
   t->waiting_for_lock = NULL;
   list_init(&t->donations);
 
-  old_level = intr_disable ();
-  list_push_back (&all_list, &t->allelem);
-  intr_set_level (old_level);
+  //old_level = intr_disable ();
+  
+  //intr_set_level (old_level);
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
